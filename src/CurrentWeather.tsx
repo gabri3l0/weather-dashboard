282f2ce57d5c4702e46e3ea3c2f04d13b1799e23
Gabriel Soto <gabriel.lopez@pidedirecto.mx>
2024-07-31 13:20:45 -0600

    fix empty current weather

--- src/CurrentWeather.tsx
+++ src/CurrentWeather.tsx
@@ -11,11 +11,28 @@ import {spacing} from "@ui5/webcomponents-react-base";
 import heart2Icon from '@ui5/webcomponents-icons/dist/heart-2.js';
 import heartIcon from '@ui5/webcomponents-icons/dist/heart.js';
 
-export function CurrentWeather({weather, handleSaveLocationClick, isLocationSaved}: {weather: any, handleSaveLocationClick:any, isLocationSaved: boolean}) {
-    if (!weather?.current) return
 
-    const date = new Date().toLocaleString('en-US', { timeZone: weather.timezone })
+type CurrentWeatherProps =
+    {
+        weather: WeatherType,
+        handleSaveLocationClick: any,
+        isLocationSaved: boolean
+    }
 
+type WeatherType ={
+    cityName: string,
+    countryCode: string,
+    current: {
+        temp: number
+        weather: [{
+            icon: string
+        }]
+    },
+    timezone?: string,
+}
+
+export function CurrentWeather({weather, handleSaveLocationClick, isLocationSaved}: CurrentWeatherProps) {
+    const date = new Date().toLocaleString('en-US', { timeZone: weather?.timezone })
     return (
         <FlexBox
             justifyContent={FlexBoxJustifyContent.Center}
@@ -28,35 +45,38 @@ export function CurrentWeather({weather, handleSaveLocationClick, isLocationSave
                     justifyContent="SpaceAround"
                     alignItems={FlexBoxAlignItems.Center}
                     style={{
-                        width: '100%'
+                        width: '100%',
+                        height: '13rem'
                     }}
                 >
-
-                    {/*TODO set values*/}
-                    <div>
-                        <Text
-                            style={{
-                                display: "block",
-                                fontWeight: 'bold'
-                            }}
-                        >
-                            {weather.cityName}, {weather.countryCode}
-                        </Text>
-                        <Text>{date}</Text>
-                    </div>
-                    <Text>
-                        <img
-                            alt='weather'
-                            src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
-                            style={{width: "70%"}}
-                        />
-                    </Text>
-                    <Text>{(weather.current.temp).toFixed(0)} °C</Text>
-                    <Button
-                        design={isLocationSaved ? "Negative" : "Transparent"}
-                        icon={isLocationSaved ? heartIcon : heart2Icon}
-                        onClick={()=>handleSaveLocationClick(weather)}
-                    />
+                    {weather?.current && (
+                        <>
+                            <div>
+                                <Text
+                                    style={{
+                                        display: "block",
+                                        fontWeight: 'bold'
+                                    }}
+                                >
+                                    {weather.cityName}, {weather.countryCode}
+                                </Text>
+                                <Text>{date}</Text>
+                            </div>
+                            <Text>
+                                <img
+                                    alt='weather'
+                                    src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
+                                    style={{width: "70%"}}
+                                />
+                            </Text>
+                            <Text>{(weather.current.temp).toFixed(0)} °C</Text>
+                            <Button
+                                design={isLocationSaved ? "Negative" : "Transparent"}
+                                icon={isLocationSaved ? heartIcon : heart2Icon}
+                                onClick={()=>handleSaveLocationClick(weather)}
+                            />
+                        </>
+                    )}
                 </FlexBox>
             </Card>
         </FlexBox>
