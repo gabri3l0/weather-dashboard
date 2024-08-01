dce907dac990871957f499fb90c4c55fddf2401e
Gabriel Soto <gabriel.lopez@pidedirecto.mx>
2024-07-31 18:34:49 -0600

    split forecasted Weather

--- src/components/ForecastedWeather/ForecastedWeather.tsx
+++ src/components/ForecastedWeather/ForecastedWeather.tsx
@@ -22,9 +22,6 @@ type ForecastedWeatherType = {
     ]
 }
 
-// FORECASTED LIST
-// FORECASTEDITEM
-
 export function ForecastedWeather({forecastedWeather}: ForecastedWeatherProps) {
     return (
         <FlexBox
@@ -36,32 +33,40 @@ export function ForecastedWeather({forecastedWeather}: ForecastedWeatherProps) {
                     {forecastedWeather ? (
                         <>
                             {forecastedWeather?.slice(1,6).map((weather) => {
-                                const dayName = new Date(weather.dt * 1000).toLocaleString('en-us', {weekday:'long'})
-                                return(
-                                    <FlexBox key={weather.dt}
-                                             direction="Column"
-                                             alignItems="Center"
-                                    >
-                                        <Text>
-                                            <img
-                                                alt='weather'
-                                                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                                                style={{width: "6em"}}
-                                            />
-                                        </Text>
-                                        <Text style={{fontWeight: 'bold'}}>
-                                            {`${(weather.temp.min).toFixed(0)}°C / ${(weather.temp.max).toFixed(0)}°C`}
-                                        </Text>
-                                        <Text style={{fontWeight: 'bold', fontSize:"1.2em", ...spacing.sapUiContentPadding}}>
-                                            {dayName}
-                                        </Text>
-                                    </FlexBox>
+                                return (
+                                    <div key={weather.dt}>
+                                        <ForecastedWeatherCard weather={weather}/>
+                                    </div>
                                 )
                             })}
                         </>
                     ): <IllustratedMessage name="NoData" size="Base"/>}
                 </FlexBox>
             </Card>
+        </FlexBox>
+    )
+}
+
+function ForecastedWeatherCard({weather}: {weather: ForecastedWeatherType}) {
+    const dayName = new Date(weather.dt * 1000).toLocaleString('en-us', {weekday:'long'})
+    return(
+        <FlexBox key={weather.dt}
+                 direction="Column"
+                 alignItems="Center"
+        >
+            <Text>
+                <img
+                    alt='weather'
+                    src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                    style={{width: "6em"}}
+                />
+            </Text>
+            <Text style={{fontWeight: 'bold'}}>
+                {`${(weather.temp.min).toFixed(0)}°C / ${(weather.temp.max).toFixed(0)}°C`}
+            </Text>
+            <Text style={{fontWeight: 'bold', fontSize:"1.2em", ...spacing.sapUiContentPadding}}>
+                {dayName}
+            </Text>
         </FlexBox>
     )
 }
